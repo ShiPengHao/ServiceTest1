ce75feff80c6a8fcc083300723f3eca5fea5bbc1
StoneLeranAndroid <13526791849@163.com>
2017-05-15 17:30:29 +0800

    ccc

--- app/src/main/java/com/yimeng/servicetest/service/CountIntentService.java
+++ app/src/main/java/com/yimeng/servicetest/service/CountIntentService.java
@@ -47,6 +47,7 @@ public class CountIntentService extends IntentService {
         // 模拟操作：计数10次
         int count = 0;
         int MAX_COUNT = 10;
+
         do {
             SystemClock.sleep(1000);
             if (runningFlag) {
